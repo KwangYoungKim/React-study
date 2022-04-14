67138c78f3944dee574ffa881b3993e25e10d65f
김광용 <lightdragonkim@gmail.com>
2022-04-14 10:14:44 +0900

    main 화면 수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let hellow ='안녕하세요!. react'
+  let hellow = '안녕하세요!. react'
   return (
     <div className="App">
       <header className="App-header">
-{/*         <img src={logo} className="App-logo" alt="logo" /> */}
+        {/*         <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           {hellow}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p>        
+           Learn React
+           Git merage Test
       </header>
     </div>
   );
