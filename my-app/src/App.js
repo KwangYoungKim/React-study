9f784f55b6c76b60d8e3e3c3d3770b99a8f9f255
김광용 <lightdragonkim@gmail.com>
2022-04-14 09:58:44 +0900

    이미지제거 및 JSX 기본 익히기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let hellow ='안녕하세요!. react'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+{/*         <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {hellow}
         </p>
         <a
           className="App-link"
